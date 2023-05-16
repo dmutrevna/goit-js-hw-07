b8de0f390e333cfe4d679042277c2b637323043b
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-05-16 16:25:12 +0200

    update

---
+++ js/01-gallery.js
@@ -0,0 +1,37 @@
+import { galleryItems } from './gallery-items.js'
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery')
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, description, original }) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>
+  `
+  )
+  .join('')
+
+galleryContainer.insertAdjacentHTML(`beforeend`, galleryMarkup)
+
+galleryContainer.addEventListener('click', (event) => {
+  event.preventDefault()
+  if (event.target.nodeName !== 'IMG') {
+    return
+  }
+
+  const originalSource = event.target.dataset.source
+  const instance = basicLightbox.create(`
+    <img src="${originalSource}">
+  `)
+  instance.show()
+})
+console.log(galleryItems)
---
+++ js/02-lightbox.js
@@ -0,0 +1,30 @@
+import { galleryItems } from './gallery-items.js'
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery')
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, description, original }) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>
+</li>
+  `
+  )
+  .join('')
+
+galleryContainer.insertAdjacentHTML(`beforeend`, galleryMarkup)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+})
+
+console.log(galleryItems)
